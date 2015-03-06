fcb0485566b87b6fdab28f5d16d54e41d1c8e030
quent01 <quentinthenoz@gmail.com>
2015-03-06 08:02:58 +0100

    Ajout du numéro 13

--- index.php
+++ index.php
@@ -1,122 +1,271 @@
 <!DOCTYPE html>
+
 <html>
+
 	<head>
+
 		<meta charset="utf-8"/>
+
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<link rel="icon" href="Images/Icones/favicon.png">
+
 		<link rel="stylesheet" href="css/reset.css"/>
+
 		<link rel="stylesheet" href="css/styledailiie.css" />
+
 		<link rel="alternate" type="application/rss+xml" href="https://dailiieplanet.iiens.eu/feed/rss.xml" />
+
+
 
 		<?php include("google_analytics.php"); ?>
 
+
+
 		<title>Dailiie Planet</title>
 
+
+
 		<!--[if lt IE 9]>
+
         	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+
         <![endif]-->
+
 	</head>
 
+
+
 	<body>
+
 		<header>
+
 			<?php include("templates/_header.php"); ?>
+
 		</header>	
+
 			
 
+
+
 		<div id="main">
+
 			<article>
+
+				<h1>Dailiie #13</h1>
+
+				<div class="view shadow">  
+
+		            <img class="shadow" href="#" src="Images/Couv/Couv_moyen/DailiiePlanet13.jpg"/> 
+
+				     	<div class="mask">  
+
+				     		<h2>Dailiie #13</h2>  
+
+				     		<p>Le Dailiie Planet est de retour avec ce numéro 13 !<br> 
+
+				     		Espérons qu'il nous portera chance !</p>  
+
+				         	<a href="anciens_numeros/DailiiePlanet13.pdf" target="_blank" class="info">Read now</a>  
+
+				     	</div>  
+
+				</div>
+
+		    </article>
+
+		    <hr>
+
+
+			<article>
+
 				<h1>Dailiie #12</h1>
+
 				<div class="view shadow">  
+
 		            <img class="shadow" href="#" src="Images/Couv/Couv_moyen/DailiiePlanet12.jpg"/> 
+
 				     	<div class="mask">  
+
 				     		<h2>Dailiie #12</h2>  
+
 				     		<p>On démarre l'année en beauté avec le numéro 12 !<br> 
+
 				     		Numéro spécial inté, vous pourrez y découvrir les résultats des Strasbourg d'Or, les BD Tiramisu et bien plus encore !</p>  
+
 				         	<a href="anciens_numeros/DailiiePlanet12.pdf" target="_blank" class="info">Read now</a>  
+
 				     	</div>  
+
 				</div>
+
 		    </article>
+
+
 
 		    <hr>
 
+
+
 			<article>
+
 				<h1>Dailiie #11</h1>
+
 				<div class="view shadow">  
+
 		            <img class="shadow" href="#" src="Images/Couv/Couv_moyen/DailiiePlanet11.jpg"/> 
+
 				     	<div class="mask">  
+
 				     		<h2>Dailiie #11</h2>  
+
 				     		<p>Il était attendu, le voilà, le numéro 11 !<br> 
+
 				     		Numéro à la gloire de nos héros du BDS qui ont vaincu à Centrale Lyon, un seul mot me vient à l'esprit, 'Veni Vidi Vici' !</p>  
+
 				         	<a href="anciens_numeros/DailiiePlanet11.pdf" target="_blank" class="info">Read now</a>  
+
 				     	</div>  
+
 				</div>
+
 		    </article>
+
+
 
 		    <hr>
 
+
+
 			<article>
+
 				<h1>Dailiie #10</h1>
+
 				<div class="view shadow">  
+
 		            <img class="shadow" href="#" src="Images/Couv/Couv_moyen/DailiiePlanet10.jpg"/> 
+
 				     	<div class="mask">  
+
 				     		<h2>Dailiie #10</h2>  
+
 				     		<p>Et Bim, c'est le numéro 10 !<br> 
+
 				     		Un numéro pré campagne pour motiver les troupes et pour vous prévenir,<br>
+
 				     		pendant la campagne vous serez épié de tous cotés par le Dailiie Planet,<br>
+
 				     		gaffe aux boulettes !</p>  
+
 				         	<a href="anciens_numeros/DailiiePlanet10.pdf" target="_blank" class="info">Read now</a>  
+
 				     	</div>  
+
 				</div>
+
 		    </article>
 
+
+
 		    <hr>
 
+
+
 			<article>
+
 				<h1>Dailiie #9</h1>
+
 				<div class="view shadow">  
+
 		            <img class="shadow" href="#" src="Images/Couv/Couv_moyen/DailiiePlanet09.jpg"/> 
+
 				     	<div class="mask">  
+
 				     		<h2>Dailiie #9</h2>  
+
 				     		<p>Numéro 9 du 9 Septembre (999) il se devait d'être le numéro de l'intégration !<br> 
+
 				     		Retrouve les BD iiennes, et découvre les élèves de l'école dans des articles plus loufoques les uns que les autres !</p>  
+
 				         	<a href="anciens_numeros/DailiiePlanet9.pdf" target="_blank" class="info">Read now</a>  
+
 				     	</div>  
+
 				</div>
+
 			</article>
+
+
 
 		    <hr>
 
-			<article>
+
+
+<!-- 			<article>
+
 				<h1>Dailiie #8</h1>
+
 				<div class="view shadow">  
+
 		            <img class="shadow" href="#" src="Images/Couv/Couv_moyen/DailiiePlanet08.jpg"/> 
+
 				     	<div class="mask">  
+
 				     		<h2>Dailiie #8</h2>  
+
 				     		<p>Dernier Dailiie Planet pour l'année scolaire 2012/2013.<br> 
+
 				     		En exclu la nouvelle BD iienne, Tiramisù !</p>  
+
 				         	<a href="anciens_numeros/DailiiePlanet8.pdf" target="_blank" class="info">Read now</a>  
+
 				     	</div>  
+
 				</div>
-			</article>
+
+			</article> -->
+
+
 
 <!-- 			<h1>Dailiie #7</h1>
+
 			<article>
+
 				<div class="view shadow">  
+
 		            <img class="shadow" href="#" src="Images/Couv/Couv_moyen/DailiiePlanet07.jpg"/> 
+
 				     	<div class="mask">  
+
 				     		<h2>Dailiie #7</h2>  
+
 				     		<p>Dailiie Planet de la campagne BdE 2012/2013.<br> 
+
 				     		Découvrez la liste gagnante !</p>  
+
 				         	<a href="anciens_numeros/DailiiePlanet7.pdf" target="_blank" class="info">Read now</a>  
+
 				     	</div>  
+
 				</div>
+
 		    </article>  -->
 
 
+
+
+
 		<!-- close #main content -->
+
 		</div>
+
 		
+
 		<?php include("templates/_footer.php"); ?>
+
 	
+
 	</body>
+
 </html>
+
